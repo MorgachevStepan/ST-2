9c092ba5b807b03cabdaa290fcf9312009d492f1
Stepan Morgachev <stepan.morgachev@gmail.com>
2024-03-10 15:40:38 +0300

    feat: added tasks class

---
+++ include/tasks.h
@@ -0,0 +1,12 @@
+// Copyright 2024 Stepan Morgachev
+#ifndef ST_2_TASKS_H
+#define ST_2_TASKS_H
+
+double solvePlanetAndRopeTask(double planetRadius, double radiusIncrement);
+
+double solveSwimmingPoolTask(double poolRadius,
+                             double pathWidth,
+                             double costOfCoverage,
+                             double fencePrice);
+
+#endif //ST_2_TASKS_H
